225966af260d1f0d30eb565bd5ad759b48d7053b
Jek-San <88803418+Jek-San@users.noreply.github.com>
2023-05-21 06:06:31 +0700

    set up login page
    sss

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react"
+import Button from "../compponent/ui/Button"
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return <Button> hellos</Button>
+}
+
+export default page
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image"
+import { db } from "../lib/db"
+export default async function Home() {
+  return <div className="text-red-500">Hello worrld</div>
+}
